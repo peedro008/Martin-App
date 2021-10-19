afebd18b93c66d96fa44ef717019adb53af82dac
Tobias <tobias.marroquin@gmail.com>
2021-10-19 19:51:29 -0300

    button-ClientCart

--- Martin/screens/Client/clientCart.js
+++ Martin/screens/Client/clientCart.js
@@ -42,14 +42,14 @@ export default function clientCart() {
                         <View style={styles.container}>
                             <View style={styles.contImage}>
                                 <ImageBackground source={{uri: item.img}} style={styles.image}>
-                                    <Button
-                                    onPress={() => handleDelete(item.id)} 
-                                    title="X"
-                                    >  
-                                        <View style={styles.buttonX}>
-                                            
-                                        </View>
-                                    </Button>
+                                    <View style={styles.buttonX}>
+                                        <Button
+                                        onPress={() => handleDelete(item.id)} 
+                                        title="X"
+                                        color="#F15A4D"
+                                        />
+                                    </View>
+                                   
                                 </ImageBackground>
                             </View>
                             {/* <Button title="+" onPress={()=>handlePlus()}/>
@@ -57,7 +57,7 @@ export default function clientCart() {
                             <Button title="-" onPress={()=> handleLess(el.quantity)} disabled={el.quantity>1 ? false : true} /> */}
                             <View style={styles.info}>
                                 <Text style={styles.name}>{item.name}</Text>
-                                <Text style={styles.price}>${item.price}</Text>
+                                <Text style={styles.price}>${item.price.toFixed(2)}</Text>
                                 <View style={styles.contQuantity}>
                                     <Text>Quantity:</Text>
                                     <Text style={{color:"#777777"}}> x{item.quantity}</Text>
@@ -71,7 +71,7 @@ export default function clientCart() {
             {order.length>0 ? (
             <View style={styles.contTotalOrder}>
                 <Text style={styles.totalOrder}>
-                    Total order ${totalPrice}  
+                    Total order ${totalPrice.toFixed(2)}  
                 </Text> 
             <View style={styles.buttonOrder}>
                 <Button color="#F15A4D" title="place order" onPress={()=>handlePostOrder()}/>
@@ -89,18 +89,18 @@ const styles = StyleSheet.create({
   container:{
       display:"flex",
       flexDirection:"row",
-      marginBottom:15
+      marginBottom:15,
+      marginTop:10
   },
   buttonX:{
-      padding:10,
-      backgroundColor:"#F15A4D",
+      padding:5,
       width:10,
       height:10,
       justifyContent:"center",
       alignItems:"center",
       position:"absolute",
-      top:0,  
-      right:0,
+      top:12,  
+      right:7,
       borderRadius:3
   },
   contImage:{
