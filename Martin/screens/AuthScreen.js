a458e162699683cb0621f5413e0e0feeb48ba8d8
peedro008 <pedro39661@gmail.com>
2021-11-01 10:45:41 -0300

    fonts(front)

--- Martin/screens/AuthScreen.js
+++ Martin/screens/AuthScreen.js
@@ -132,6 +132,7 @@ const styles = StyleSheet.create({
     heading: {
         fontSize: width*0.07,
         fontWeight: '600',
+        fontFamily:"OpenSans-Bold",
         marginLeft:width*0.1,
         marginTop: width*0.13,
         color: 'black',
