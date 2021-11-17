540d1dec9a29df8853eabe79a671c8eab07a237c
peedro008 <pedro39661@gmail.com>
2021-11-17 16:58:39 -0300

    fast order

--- Martin/screens/Client/stack/homeComponents/orders.js
+++ Martin/screens/Client/stack/homeComponents/orders.js
@@ -27,7 +27,7 @@ export default function orders({navigation, data}) {
 
     const email =  useSelector(state=> state.User)
     const preOrder=useSelector(state=> state.PreOrder)
-    
+    const [last, setLast] = useState([])
     const [card, setCard] = useState()
 
     const dispatch=useDispatch()
@@ -38,7 +38,9 @@ export default function orders({navigation, data}) {
     }
     
     
-    
+    useEffect(()=>{
+        setLast(data.slice(0,2))
+    }, [data])  
     
     useEffect(() => {
         let pes = {}
@@ -73,7 +75,7 @@ export default function orders({navigation, data}) {
             <SafeAreaView
             style={{marginTop:width*.06,marginBottom:width*0.02, alignItems:'center', flex:1}}>
                 <View style={{width:width*0.9}}>   
-                    <Text style={styles.OrderHeader} >Last Orders</Text> 
+                    <Text style={styles.OrderHeader} >Fast order</Text> 
                 </View>
 
                
@@ -84,93 +86,26 @@ export default function orders({navigation, data}) {
             contentContainerStyle={{paddingBottom:width*0.1}}
             bounces={false}
             horizontal={true}
-            data={data}
+            data={last}
             renderItem={({item})=>{
-            let id=data.indexOf(item)
-
-                return(
-                <Card containerStyle={styles.card} >
-                    
-                <View style={{margin:width*0.03, alignSelf:"center"}}>
-                   <View style={{flexDirection: 'row', marginBottom:width*0.03, marginTop:-width*0.03, alignItems:'center'}}>
-                    
-                        <Text style={{color:item.status == "Pending" ? "orange" : item.status=="Received" ? "#40D3A8" : "#6979F8" , fontSize:width*0.04,fontFamily:"OpenSans-Regular", textTransform:"uppercase"}}>{item.status}</Text>
-                        <Text style={{fontFamily:"OpenSans-Regular",color:"#999999", fontWeight:"300",position:"absolute",right:0,fontSize:width*0.04 }}>{item.createdAt.substring(0,9)} | {item.createdAt.substring(11,16)}</Text>
-                    </View>
-                    <Card.Divider/>
-                    <View
-                        style={{flexDirection: 'row'}}>
-                        <Text style={{fontFamily:"OpenSans-Regular",margin:width*0.017, fontSize:width*0.065, color:"#40D3A8", fontWeight:"600"}}>
-                        Order N° {item.id} 
-                        </Text>
-                        
-                        <View
-                        style={{marginBottom:width*0.04}}>
-                           
-                        <View style={{flexDirection:"row"}}>  
-                            
-                            
-                            
-                            {card[id]?
-                            <TouchableOpacity 
-                            onPress={toCart}
-                            style={{borderRadius:5, width:width*0.2, height:width*0.065, backgroundColor:card[id]?"#6979F8":"#40D3A8",justifyContent:"center", marginLeft:width*0.23}}>
-                            <Text
-                            style={{fontFamily:"OpenSans-Regular", fontSize:width*0.026, alignSelf:"center",fontWeight:"500", color:"#fff", }}>
-                            GO TO CART
-                            </Text>
-                                                        
-                            </TouchableOpacity>:
-
-                             <TouchableOpacity 
-                             onPress={() => handleAddProduct( item.orderItems, id )}
-                             style={{borderRadius:5, width:width*0.2, height:width*0.065, backgroundColor:card[id]?"#6979F8":"#40D3A8",justifyContent:"center", marginLeft:width*0.23}}>
-                            <Text
-                             style={{fontFamily:"OpenSans-Regular", fontSize:width*0.026, alignSelf:"center",fontWeight:"500", color:"#fff", }}>
-                             ADD TO CART
-                             </Text>
-                             </TouchableOpacity>
-
-                        }
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        </View>
-                        <TouchableOpacity
-                        onPress={() => navigation.navigate("order detail",{id:item.id})}
-                        style={{borderRadius:5, width:width*0.2, height:width*0.065, marginTop:width*0.008,justifyContent:"center", backgroundColor:"#F15A4D", marginLeft:width*0.23}}>
-                            <Text
-                            style={{fontFamily:"OpenSans-Regular",fontSize:width*0.026, alignSelf:"center",fontWeight:"500", color:"#fff", }}>
-                            DETAILS
-                            </Text>
-                        </TouchableOpacity>
-                        </View>
-                      
-                    </View>
-                        <Card.Divider/>
-                        <View
-                        style={{flexDirection: 'row'}}>
-                            <View style={{flexDirection: 'row', alignItems:'center'}}>
-                                <Text
-                                style={{fontFamily:"OpenSans-Regular", fontSize: width*0.035, color:"#999999"}}>VALUE OF ITEMS: </Text>
-                                <Text style={{fontFamily:"OpenSans-Regular",fontSize:width*0.04, fontWeight:"600", color:"#151522"}}>$ {item.total.toFixed(2)}</Text>
-                            </View>
-                            <View style={{flexDirection: 'row',alignItems:'center', position:"absolute", right:0}}>
-                                <Text
-                                style={{fontFamily:"OpenSans-Regular", fontWeight:"400", fontSize: width*0.035, color:"#999999"}}>QUANTITY: </Text>
-                                <Text style={{fontFamily:"OpenSans-Regular",fontSize:width*0.04, fontWeight:"600", color:"#151522"}}>x{item.orderItems.length}</Text>
-                            </View>
-                        </View>
-                     </View>   
-                    </Card> )} 
-         
-                        }/>
+             return(
+                <View>
+                
+                <Card containerStyle={styles.card}>
+                        <View>
+                            <TouchableOpacity
+                                onPress={()=>navigation.navigate("fast Order", {order:item})}>
+                                <Image style={styles.image} source={require("../../../../assets/Check.png")}/>
+                            </TouchableOpacity>
+                        </View> 
+                        <View style={styles.textContainer}>
+                    <Text style={{fontFamily:"OpenSans-Regular"}}>{item.createdAt.substring(0,9)}</Text>
+                    <Text style={{color:"#666666", fontFamily:"OpenSans-Regular"}}>$ {item.total.toFixed(2)}</Text></View>
+                </Card>
+                </View>
+                
+                )} 
+            }/>
                       :
                       <View>
                       <Text style={{color:"grey", fontStyle:"OpenSans-Regular", paddingTop:width*0.05}}>NOT ORDERS YET</Text>
@@ -183,16 +118,20 @@ export default function orders({navigation, data}) {
 const styles = StyleSheet.create({
         OrderHeader:{
             textAlign:"center",
-            marginTop:width*0.04,
-            marginBottom:width*0.05,
+            marginVertical:width*0.08,
             fontSize: width*0.06,
         
             fontFamily:"OpenSans-SemiBold"
         },
+        image:{
+            height:60,
+            width:60,
+            alignSelf:"center"
+        },
         card:{
             
-           height:width*0.47,
-            width:width*0.9,
+           height:width*0.32,
+            width:width*0.413,
             borderRadius:5, 
             borderWidth:1, 
             borderColor:"rgba(228, 228, 228, 0.6)",
@@ -204,7 +143,12 @@ const styles = StyleSheet.create({
             shadowOpacity: 0.44,
             shadowRadius: 10.32,
             elevation: 16,
+            flexDirection:"column"
         
             
+        },
+        textContainer:{
+            alignItems:"center",
+            marginTop:width*0.02
         }
     })
